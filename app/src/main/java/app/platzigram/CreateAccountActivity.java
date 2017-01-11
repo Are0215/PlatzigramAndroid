d8524b4e9a674b56287761cf143bc6818098353f
Andres Rodriguez <andres.rodriguez0215@gmail.com>
2017-01-10 22:45:44 -0500

    first commit

---
+++ app/src/main/java/app/platzigram/CreateAccountActivity.java
@@ -0,0 +1,25 @@
+package app.platzigram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showToolbar("Crear cuenta", true);
+    }
+
+
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setHomeButtonEnabled(upButton);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+    }
+}
---
+++ app/src/main/java/app/platzigram/LoginActivity.java
@@ -0,0 +1,21 @@
+package app.platzigram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void createAccount(View v){
+        Intent it = new Intent(this, CreateAccountActivity.class);
+        startActivity(it);
+
+    }
+}
